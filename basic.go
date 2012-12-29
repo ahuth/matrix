08ab171a70d604d5a7c10e656c60b6adea56688a
ahuth <andrewhuth2@live.com>
2012-12-28 20:23:21 -0500

    Split creation functions into new file

--- basic.go
+++ basic.go
@@ -39,37 +39,6 @@ func (A *Matrix) Row(n int) []int {
 	return A.data[findIndex(n, 1, A):findIndex(n, A.columns+1, A)]
 }
 
-// Identity creates an identity matrix with n rows and n columns.  When you
-// multiply any matrix by its corresponding identity matrix, you get the
-// original matrix.
-
-func Identity(n int) Matrix {
-	A := Zeros(n, n)
-	for i := 0; i < len(A.data); i += (n + 1) {
-		A.data[i] = 1
-	}
-	return A
-}
-
-// Zeros creates an r x c sized matrix that's filled with zeros.  The initial
-// state of an int is 0, so we don't have to do any initialization.
-
-func Zeros(r, c int) Matrix {
-	return Matrix{r, c, make([]int, r*c)}
-}
-
-// New creates an r x c sized matrix that is filled with the provided data.
-// The matrix data is represented as one long slice.
-
-func New(r, c int, data []int) Matrix {
-	if len(data) != r*c {
-		panic("[]int data provided to matrix.New is greater than the provided capacity of the matrix!'")
-	}
-	A := Zeros(r, c)
-	A.data = data
-	return A
-}
-
 // findIndex takes a row and column and returns the corresponding index
 // from the underlying data slice.
 
---
+++ create.go
@@ -0,0 +1,32 @@
+package matrix
+
+// Identity creates an identity matrix with n rows and n columns.  When you
+// multiply any matrix by its corresponding identity matrix, you get the
+// original matrix.
+
+func Identity(n int) Matrix {
+	A := Zeros(n, n)
+	for i := 0; i < len(A.data); i += (n + 1) {
+		A.data[i] = 1
+	}
+	return A
+}
+
+// Zeros creates an r x c sized matrix that's filled with zeros.  The initial
+// state of an int is 0, so we don't have to do any initialization.
+
+func Zeros(r, c int) Matrix {
+	return Matrix{r, c, make([]int, r*c)}
+}
+
+// New creates an r x c sized matrix that is filled with the provided data.
+// The matrix data is represented as one long slice.
+
+func New(r, c int, data []int) Matrix {
+	if len(data) != r*c {
+		panic("[]int data provided to matrix.New is greater than the provided capacity of the matrix!'")
+	}
+	A := Zeros(r, c)
+	A.data = data
+	return A
+}
